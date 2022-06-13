59354696a67a2df03f43c3003714af7fbf06afc8
mblester <morganmurry@gmail.com>
2022-06-13 11:04:50 -0500

    what we did in class

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports = [
+
+
+	{
+        "id": "1",
+		"name": "Alisa Summers",
+		"phone": "(892) 434-8675",
+		"email": "placerat.orci.lacus@protonmail.couk",
+		"postalZip": "51214"
+	},
+	{
+        "id": "2",
+		"name": "Hyacinth Andrews",
+		"phone": "(369) 311-9341",
+		"email": "quis.diam.pellentesque@yahoo.couk",
+		"postalZip": "86862"
+	},
+	{
+        "id": "3",
+		"name": "Kennan Mccray",
+		"phone": "1-935-633-2170",
+		"email": "ac@google.net",
+		"postalZip": "44165"
+	},
+	{
+        "id": "4",
+		"name": "Cullen Hays",
+		"phone": "(622) 325-1135",
+		"email": "pharetra.sed@protonmail.ca",
+		"postalZip": "1573-8828"
+	},
+	{
+        "id": "5",
+		"name": "Jelani Curry",
+		"phone": "1-649-366-7838",
+		"email": "dis.parturient.montes@icloud.com",
+		"postalZip": "83573-64934"
+	}
+]
--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+//get data
+const data = require('./data');
 
-const { users } = require('./state')
+// let fakeNewPerson = {
+//   id: 6,
+//   "name": "I am Sam",
+//   "phone": "1111",
+//   "email": "fake@fake.com",
+//   "postalZip": "85020"
+// };
 
-/* BEGIN - create routes here */
+app.use(express.json())
 
+const port = 3000;
 
-/* END - create routes here */
+//get all people resource
+app.get('/people', (req, res) => {
+  res.json(data)
+})
+
+//get one person resource
+app.get("/people/:id", (req, res) => {
+  const id = req.params.id;
+  const foundItem = data.find((item, index, arr) => item.id === +req.params.id);
+
+  res.json(foundItem)
+  // const person = people.find((person) => person.id === Number(id));
+  // res.json(person);
+});
+
+app.post("/people", (req, res) => {
+  const length = data.length;
+  const newPerson = {
+    id: length + 1,
+    ...req.body,
+  }
+  data.push(newPerson)
+  res.json(data)
+});
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
